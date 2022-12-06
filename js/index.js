9903613677ffbc64e65315291911a488edd1860a
mikegdro <miguelgdro@gmail.com>
2022-12-06 13:00:37 +0100

    hotfix

--- js/index.js
+++ js/index.js
@@ -1,37 +1,38 @@
-window.onload(() => {
-//Mapa
-const map = L.map('map').setView([56.4906712, -4.2026458], 6);
+window.onload = function () {
+    console.log("hi")
+    //Mapa
+    const map = L.map('map').setView([56.4906712, -4.2026458], 6);
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-}).addTo(map);
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        maxZoom: 19,
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+    }).addTo(map);
 
-function onMapClick(e) {
-    alert("You clicked the map at " + e.latlng);
-};
+    function onMapClick(e) {
+        alert("You clicked the map at " + e.latlng);
+    };
 
-map.on('click', onMapClick);
-
-
-var marker = L.marker([57.48759, -4.221497]).addTo(map);
+    map.on('click', onMapClick);
 
 
-// Swiper
-const swiper = new Swiper('.swiper', {
-    speed: 400,
-    spaceBetween: 100,
+    var marker = L.marker([57.48759, -4.221497]).addTo(map);
 
-    pagination: {
-        el: '.swiper-pagination',
-        type: 'bullets',
-        clickable: true,
-    },
 
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev'
-    }
-});
-})
+    // Swiper
+    const swiper = new Swiper('.swiper', {
+        speed: 400,
+        spaceBetween: 100,
+
+        pagination: {
+            el: '.swiper-pagination',
+            type: 'bullets',
+            clickable: true,
+        },
+
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev'
+        }
+    });
+}
 
